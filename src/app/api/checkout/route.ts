bab4b64edb8e340015061a2cb3f3e1c50ea40470
Amanda Tyo:) <a@Gilang-Prasetyo.local>
2025-09-28 01:36:18 +0800

    update: perbaikan page.tsx, BannerSlider, EventList dan tambah folder api/events/success/failed

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -26,8 +26,8 @@ export async function POST(req: Request) {
         amount: total,
         payer_email: "customer@email.com", // TODO: ambil dari login form
         description: `Pembelian tiket untuk event ${eventId}`,
-        success_redirect_url: "https://hai-ticket.netlify.app", // ganti domain prod
-        failure_redirect_url: "https://hai-ticket.netlify.app",
+        success_redirect_url: "https://hai-ticket.netlify.app/success", // ganti domain prod
+        failure_redirect_url: "https://hai-ticket.netlify.app/failed",
       }),
     });
 
